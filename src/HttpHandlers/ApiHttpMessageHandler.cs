098a2617186d2a663f7f1ebdcd197cfe292df1a4
Paolo Argentieri <paolo.argentieri@laserfiche.com>
2024-05-20 12:06:29 -0700

    Nuget

--- src/HttpHandlers/ApiHttpMessageHandler.cs
+++ src/HttpHandlers/ApiHttpMessageHandler.cs
@@ -1,37 +1,25 @@
 // Copyright (c) Laserfiche.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
+using Laserfiche.Api.Client.HttpHandlers;
+using Laserfiche.Api.Client.Utils;
 using System;
 using System.Net;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
-using Laserfiche.Api.Client.Utils;
 
-namespace Laserfiche.Api.Client.HttpHandlers
+namespace Laserfiche.Repository.Api.Client
 {
-    /// <summary>
-    /// HttpClient Message Handler to interact with Laserfiche APIs. It provides the following features:
-    /// - Uses the IHttpRequestHandler to obtain the value of the Authorization header and refresh it as needed.
-    /// - Sets the Api Base Address based on the Laserfiche cloud region Access Token.
-    /// - Built-in retries on 401 and transient errors.
-    /// </summary>
-    public class ApiHttpMessageHandler : DelegatingHandler
+    internal class RepositoryApiClientHandler : DelegatingHandler
     {
-        public delegate string GetApiBaseUri(string laserficheCloudRegionalDomain);
         private readonly IHttpRequestHandler _httpRequestHandler;
-        private readonly GetApiBaseUri _getApiBaseUri;
+        private readonly string _baseUrlDebug;
+        private Uri _baseAddress;
 
-        /// <summary>
-        /// Constructor.
-        /// </summary>
-        /// <param name="httpRequestHandler"></param>
-        /// <param name="getApiBaseUri">Typical argument values: <see cref="DomainUtils.GetRepositoryApiBaseUri"/>, <see cref="DomainUtils.GetODataApiBaseUri"/>, or a function returning a custom API base address.</param>
-        /// <exception cref="ArgumentNullException"></exception>
-        public ApiHttpMessageHandler(IHttpRequestHandler httpRequestHandler, GetApiBaseUri getApiBaseUri) :
-          base(new HttpClientHandler() { AutomaticDecompression = DecompressionMethods.GZip })
+        public RepositoryApiClientHandler(IHttpRequestHandler httpRequestHandler, string baseUrlDebug) : base(new HttpClientHandler() { AutomaticDecompression = DecompressionMethods.GZip })
         {
             _httpRequestHandler = httpRequestHandler ?? throw new ArgumentNullException(nameof(httpRequestHandler));
-            _getApiBaseUri = getApiBaseUri ?? throw new ArgumentNullException(nameof(getApiBaseUri));
+            _baseUrlDebug = baseUrlDebug;
         }
 
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
@@ -46,8 +34,12 @@ namespace Laserfiche.Api.Client.HttpHandlers
 
             // Sets the authorization header
             var beforeSendResult = await _httpRequestHandler.BeforeSendAsync(request, cancellationToken).ConfigureAwait(false);
-            Uri apiBaseUri = new Uri(_getApiBaseUri(beforeSendResult.RegionalDomain));
-            request.RequestUri = new Uri(apiBaseUri, request.RequestUri.PathAndQuery);
+
+            if (_baseAddress == null || (_baseUrlDebug == null && !_baseAddress.Host.EndsWith(beforeSendResult.RegionalDomain)))
+            {
+                _baseAddress = GetBaseAddress(beforeSendResult.RegionalDomain);
+            }
+            request.RequestUri = new Uri(_baseAddress, request.RequestUri.PathAndQuery);
 
             try
             {
@@ -76,6 +68,13 @@ namespace Laserfiche.Api.Client.HttpHandlers
             }
             return response;
         }
+
+        private Uri GetBaseAddress(string domain)
+        {
+            string baseAddress = _baseUrlDebug ?? DomainUtils.GetRepositoryApiBaseUri(domain);
+            return new Uri(baseAddress);
+        }
+
         private static bool IsTransientHttpStatusCode(HttpStatusCode statusCode)
         {
             return (int)statusCode >= 500 || statusCode == HttpStatusCode.RequestTimeout;
